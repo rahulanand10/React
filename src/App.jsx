1e375057a3e36e891a770c1c4f1cccf64a68d5a7
Rahul Anand <rahulanand0910@gmail.com>
2020-08-18 23:46:51 +0530

    challenge-3 with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
-import Heading from './Headings'
-import Para from './Para'
-import List from './List'
 
 function App(){
+    let curDate=new Date(2020,5,5,13)
+    curDate=curDate.getHours()
+    let greeting='';
+    const cssStyle={ };
+    
+    if (curDate>=1 && curDate<12){
+      greeting='Good Morning';
+      cssStyle.color='Green';
+    }
+    else if(curDate>=12 && curDate<19){
+      greeting='Good Afternoon';
+      cssStyle.color='Orange'
+    }
+    else{
+      greeting='Good Night'
+      cssStyle.color='Blue'
+    }
+
     return(
-        <React.Fragment>
-            <Heading/>
-            <Para/>
-            <List/>
-        </React.Fragment>
+        <div className="challenge">
+            <h1 className="heading">Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
+        </div>
     )
-};
+}
 
 export default App;
