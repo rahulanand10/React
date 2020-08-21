41d8935016afe8a068f3da577c2c5473ba72faf2
Rahul Anand <rahulanand0910@gmail.com>
2020-08-21 09:05:06 +0530

    ternary operator added

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,13 @@ import Amazon from './Amazon'
 import './index.css'
 import Netflix from './Netflix';
 
-const favSeries='amazon'
-const Favs=()=>{
-if (favSeries==='netflix'){
-    return (<Netflix/>)
-}
-else{
-  return(<Amazon/>)
-}
-}
+const favSeries='netflix'
 
 const App=()=>{
   return(
     <React.Fragment>
     <h1 className="heading_style">List of top 5 Netflix series in 2020</h1>
-    <Favs/>
+    {(favSeries==='netflix')?<Netflix/>:<Amazon/>}
   </React.Fragment>
   )
 }
