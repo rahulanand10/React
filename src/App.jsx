e9b128fb676fe85045005c67b79ed6339a1ad5fd
Rahul Anand <rahulanand0910@gmail.com>
2020-08-22 13:49:08 +0530

    digitalclock-35

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import React,{useState} from 'react';
 
 const App=()=>{
   let curr=new Date().toLocaleTimeString()
-  const [date,settime]=useState(curr);
+  const [time,set]=useState(curr);
+  setInterval(() => {
+    let curr=new Date().toLocaleTimeString()
+    set(curr)
+  }, 1000);
 
-  const setTime=()=>{
-    let cur=new Date().toLocaleTimeString()
-    settime(cur);
-}
   return(
     <React.Fragment>
-      <h1 style={{fontSize:'40px',fontWeight:'bold'}}>{date}</h1>
-      <button onClick={setTime}> Get Time</button>
+      <h1 style={{fontSize:'60px',fontWeight:'bold'}}>{time}</h1>
     </React.Fragment>
   )
 };
