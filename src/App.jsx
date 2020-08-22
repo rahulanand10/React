e406911517a0a7fa5649a3e678018979e291d909
Rahul Anand <rahulanand0910@gmail.com>
2020-08-22 14:14:56 +0530

    handling-events-37

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React,{useState} from 'react';
 
 
-const App=()=>{
-  let curr=new Date().toLocaleTimeString()
-  const [time,set]=useState(curr);
-  setInterval(() => {
-    let curr=new Date().toLocaleTimeString()
-    set(curr)
-  }, 1000);
 
+const App=()=>{
+  const [bg,setBg]=useState('purple')
+  const[name,setName]=useState('Click me');
+  const bgChange=()=>{
+    setBg('#34495e')
+    setName('OUCH!! 😢')
+  }
+  const dbChange=()=>{
+    setBg('purple')
+    setName('Ayyo !! 😠')
+  }
   return(
     <React.Fragment>
-      <h1 style={{fontSize:'60px',fontWeight:'bold'}}>{time}</h1>
+      <div style={{backgroundColor:bg }}>
+        <button onClick={bgChange} onDoubleClick={dbChange}> {name} </button>
+      </div>
     </React.Fragment>
   )
-};
+}
 
 export default App;
