2144fccfa5fc07be301d7ec4faad114cef8a0420
Rahul Anand <rahulanand0910@gmail.com>
2020-08-24 09:32:02 +0530

    project-todo-44

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,47 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import ToDoLists from './ToDoLists';
 
 const App=()=>{
+  const[inputList,setInputList]=useState()
+  const [Items,setItems]=useState([])
 
-  const [fullName,setFullName]=useState({
-    fname:'',
-    lname:'',
-    email:'',
-    phone:''
-  });
+  const itemEvent=(event)=>{
+    setInputList(event.target.value)
+  }
 
-
-  const InputEvent=(event)=>{ 
-    console.log(event.target.value)
-    console.log(event.target.name)
-
-    const {name,value}=event.target
-
-    setFullName((preValue)=>{
-
-      return{
-        ...preValue,
-        [name]:value,
-      }
-      // console.log(preValue)
-      // if(name === "fname"){
-      //   return{
-      //     fname:value,
-      //     lname:preValue.lname,
-      //     email:preValue.email,
-      //     phone:preValue.phone
-      //   };
-      // }else if(name === "lname"){
-      //   return{
-      //     fname:preValue.fname,
-      //     lname:value,
-      //     email:preValue.email,
-      //     phone:preValue.phone
-      //   };
-      // }else if(name === "email"){
-      //   return{
-      //     fname:preValue.fname,
-      //     lname:preValue.lname,
-      //     email:value,
-      //     phone:preValue.phone
-      //   };
-      // }else if(name === "phone"){
-      //   return{
-      //     fname:preValue.fname,
-      //     lname:preValue.lname,
-      //     email:preValue.email,
-      //     phone:value,
-      //   };
-      // }
+  const listofItems=()=>{
+    setItems((oldItems)=>{
+      return [...oldItems,inputList]
+    })
+    setInputList('')
+  }
+  const deleteItem=(id)=>{
+    console.log('del')
+    setItems((oldItems)=>{
+      return oldItems.filter((arrElem,index)=>{
+        return index!== id;
+      })
     })
   }
- 
-  const onSubmits=(event)=>{
-    event.preventDefault();
-    alert('form submitted')
-  }
-
   return(
     <React.Fragment>
       <div className="main_div">
-        <form onSubmit={onSubmits}>
-          <div className="nmain">
-            <h1>Hello {fullName.fname} {fullName.lname}</h1>
-            <p>{fullName.email}</p>
-            <p>{fullName.phone}</p>
-            
-            <input type='text' placeholder="Enter Your Name"
-            name="fname"
-            onChange={InputEvent} 
-            value={fullName.fname}
-            />
-            <br/>
-            <input type='text' placeholder="Enter Your Last Name"
-            name="lname"
-            onChange={InputEvent} 
-            value={fullName.lname}
-            />
-            <br/>
-            <input type='email' placeholder="Enter Your Email"
-            name="email"
-            onChange={InputEvent} 
-            value={fullName.email}
-            />
-            <br/>
-            <input type='number' placeholder="Enter Your Number"
-            name="phone"
-            onChange={InputEvent} 
-            value={fullName.phone}
-            />
-            <button>Click me</button>
-          </div>
-        </form>
+        <div className="center_div">
+          <br/>
+          <h1>ToDo List</h1>
+          <br/>
+          <input type="text" placeholder="Add a Item" 
+          value={inputList}
+          onChange={itemEvent}/>
+          <button onClick={listofItems}>+</button>
+
+          <ol>
+            {Items.map((itemval,index)=>{
+              return  <ToDoLists key={index} text={itemval} id={index}
+              onSelect={deleteItem}/>;
+            })}
+          </ol>
+        </div>
       </div>
     </React.Fragment>
   )
