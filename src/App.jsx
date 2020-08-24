118746d9f27dd263575155eb28802f7e872d0043
Rahul Anand <rahulanand0910@gmail.com>
2020-08-24 09:50:35 +0530

    challenge-8-45

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,30 @@
 import React, { useState } from 'react';
-import ToDoLists from './ToDoLists';
+
 
 const App=()=>{
-  const[inputList,setInputList]=useState()
-  const [Items,setItems]=useState([])
 
-  const itemEvent=(event)=>{
-    setInputList(event.target.value)
-  }
+  const [number,setNumber]=useState(0)
 
-  const listofItems=()=>{
-    setItems((oldItems)=>{
-      return [...oldItems,inputList]
-    })
-    setInputList('')
+  const Increment=()=>{
+    setNumber(number+1)
   }
-  const deleteItem=(id)=>{
-    console.log('del')
-    setItems((oldItems)=>{
-      return oldItems.filter((arrElem,index)=>{
-        return index!== id;
-      })
-    })
+  const Decrement=()=>{
+    if (number===0){
+      alert('Value is 0')
+    }
+    else{
+      setNumber(number-1)
+    }
   }
   return(
     <React.Fragment>
       <div className="main_div">
         <div className="center_div">
           <br/>
-          <h1>ToDo List</h1>
+          <h1>{number}</h1>
           <br/>
-          <input type="text" placeholder="Add a Item" 
-          value={inputList}
-          onChange={itemEvent}/>
-          <button onClick={listofItems}>+</button>
-
-          <ol>
-            {Items.map((itemval,index)=>{
-              return  <ToDoLists key={index} text={itemval} id={index}
-              onSelect={deleteItem}/>;
-            })}
-          </ol>
+          <button onClick={Increment}>Increm</button>
+          <button onClick={Decrement}>Decrem</button>
         </div>
       </div>
     </React.Fragment>
