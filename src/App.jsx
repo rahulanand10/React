5489d2647b8f52dcd51390895aa39bfc90ae5191
Rahul Anand <rahulanand0910@gmail.com>
2020-08-26 08:40:20 +0530

    UseEffect-57

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import React, { createContext } from 'react'
-import ComA from './ComA'
+import React, { useState, useEffect } from 'react'
 
-const FirstName=createContext();
-const LastName=createContext();
 const App=()=>{
+  const [num,setNum]=useState(0)
+  const [nums,setNums]=useState(2)
+
+  useEffect(()=>{
+    alert('I am clicked')
+  },[num])
+
+  const Increment=()=>{
+    setNum(num+1);
+  }
+  const Decrement=()=>{
+    setNums(nums+1);
+  }
   return(
     <React.Fragment>
-      <FirstName.Provider value="Rahul">
-        <LastName.Provider value="Anand">
-          <ComA/>
-        </LastName.Provider>
-      </FirstName.Provider>
+      <button onClick={Increment}><h1>Click me {num}</h1></button>
+      <button onClick={Decrement}><h1>Click now {nums}</h1></button>
     </React.Fragment>
   )
 }
 export default App;
-export {FirstName,LastName}
