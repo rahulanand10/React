9d1a402768bf5eea2aeff42df2e7592309882710
Rahul Anand <rahulanand0910@gmail.com>
2020-08-26 08:24:22 +0530

    UseContext-56

--- src/ComB.jsx
+++ src/ComB.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ComC from './ComC'
+import {FirstName,LastName} from './App'
+
 const ComB=()=>{
+  const fname=useContext(FirstName)
+  const lname=useContext(LastName)
   return(
     <React.Fragment>
+        <h1>FROM USE CONTEXT {fname} {lname}</h1>
         <ComC/>
     </React.Fragment>
   )
