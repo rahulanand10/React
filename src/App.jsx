9ad75188f2f64ef587701bdc6bb67b921e95f269
Rahul Anand <rahulanand0910@gmail.com>
2020-08-26 08:54:15 +0530

    UseEffect-challenge-58

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,17 @@ import React, { useState, useEffect } from 'react'
 
 const App=()=>{
   const [num,setNum]=useState(0)
-  const [nums,setNums]=useState(2)
 
   useEffect(()=>{
-    alert('I am clicked')
+    document.title=`You clicked me ${num} times`
   },[num])
 
   const Increment=()=>{
     setNum(num+1);
   }
-  const Decrement=()=>{
-    setNums(nums+1);
-  }
   return(
     <React.Fragment>
       <button onClick={Increment}><h1>Click me {num}</h1></button>
-      <button onClick={Decrement}><h1>Click now {nums}</h1></button>
     </React.Fragment>
   )
 }
