09772971e0465fe326a2aa7564ebb820b98f1214
Rahul Anand <rahulanand0910@gmail.com>
2020-08-26 09:52:44 +0530

    RestApi-Pokemonapi-59

--- src/ComA.jsx
+++ src/ComA.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import ComB from './ComB';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 const ComA=()=>{
+  const [num,setNum]=useState();
+  const [name,setName]=useState();
+  const [moves,setMoves]=useState();
+
+  useEffect(()=>{
+    async function getData(){
+      const res=await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
+      console.log(res.data.name);
+      setName(res.data.name)
+      setMoves(res.data.moves.length)
+    }
+    getData();
+  })
   return(
     <React.Fragment>
-        <ComB/>
+      <h1>You choose <span style={{color:'red'}}>{num}</span> value</h1>
+      <h1>My name is <span style={{color:'red'}}>{name}</span></h1>
+      <h1>I have <span style={{color:'red'}}>{moves}</span> moves</h1>
+        <select value={num} onChange={(event)=>{
+          setNum(event.target.value)
+        }}>
+          <option value="1">1</option>
+          <option value="25">25</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
     </React.Fragment>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css'
-import App from './App';
+import ComA from './ComA';
 
 
 
 ReactDOM.render(
-  <App/>,
+  <ComA/>,
   document.getElementById('root')
 )
