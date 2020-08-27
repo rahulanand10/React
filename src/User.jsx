b62a28e1457aa43530d71f32a1c528daad137b24
Rahul Anand <rahulanand0910@gmail.com>
2020-08-27 11:50:14 +0530

    Reactrouter-Usehistoryhooks-69

--- src/User.jsx
+++ src/User.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import {useParams,useLocation} from 'react-router-dom'
+import {useParams,useLocation,useHistory} from 'react-router-dom'
 
 const User=()=>{
   const {fname,lname}=useParams();
-  const location=useLocation()
+  const location=useLocation();
+  const history=useHistory();
   console.log(location.pathname);
   return(
       <React.Fragment>
         <h1>user {fname} {lname} page</h1>
         <h3>Your current location is {location.pathname}</h3>
         {location.pathname===`/user/rahul/anand`?
-        <button onClick={()=>{alert('You are awesome')}}>Click Me</button>:null
+        <button onClick={()=> history.goBack()}>Go back</button>:null
     }
+   {/* {location.pathname===`/user/rahul/anand`?
+    <button onClick={()=> history.push('/')}>Home page</button>:null}
+*/}
       </React.Fragment>
       ) 
 
