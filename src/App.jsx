1a1065c5c4e26143ae54ae58fcb30c890656f456
Rahul Anand <rahulanand0910@gmail.com>
2020-08-27 15:16:32 +0530

    Redirect and 404erropage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Route,Switch} from 'react-router-dom'
+import {Route,Switch,Redirect} from 'react-router-dom'
 import About from './About'
 import Home from './Home'
 import Contact from './Contact'
-import Error from './Error'
+// import Error from './Error'
 import Navbar from './Navbar';
 import User from "./User";
 import Search from './Search'
@@ -22,7 +22,8 @@ const App=()=>{
         <Route exact path='/contact' render={()=><Contact name="Contact"/>}/>
         <Route exact path='/contact/name' component={Name}/>
         <Route exact path='/user/:fname/:lname' component={User}/>
-        <Route component={Error}/>
+        {/*<Route component={Error}>*/}
+        <Redirect to="/"/>
       </Switch>
     </React.Fragment>
   ) 
--- src/Error.jsx
+++ src/Error.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Error=()=>{
-  return <h1>Oops! Page Not Found</h1>
+  return (
+    <React.Fragment>
+      <h1>Oops 404! Page Not Found</h1>
+      <NavLink to="/">Go Back</NavLink>
+    </React.Fragment>
+  )
 }
 
 export default Error;
