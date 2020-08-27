14a205eac16337ba97c45d0085b654d20cc48c18
Rahul Anand <rahulanand0910@gmail.com>
2020-08-27 11:30:37 +0530

    Reactrouter-Uselocationhooks-67

--- src/User.jsx
+++ src/User.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import {useParams,useLocation} from 'react-router-dom'
 
-// const User=({match})=>{
-//   return <h1>user {match.params.name} page</h1>
-// }
 const User=()=>{
   const {fname,lname}=useParams();
-  return <h1>user {fname} {lname} page</h1>
+  const location=useLocation()
+  console.log(location.pathname);
+  return(
+      <React.Fragment>
+        <h1>user {fname} {lname} page</h1>
+        <h3>Your current location is {location.pathname}</h3>
+        {location.pathname===`/user/rahul/anand`?
+        <button onClick={()=>{alert('You are awesome')}}>Click Me</button>:null
+    }
+      </React.Fragment>
+      ) 
+
 }
 
 export default User;
